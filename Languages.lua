5f908a657db5572b627b3b046142787657df807d
DiableNoir-FFXI <grandjohn87@gmail.com>
2026-03-14 21:37:19 +0100

    Add 46 languages to language file
    Added support for 46 languages in the language file to enable multi-language translations.
    This update works with the new per-language cache system and prepares the add-on for full internationalization.

--- Languages.lua
+++ Languages.lua
@@ -1,12 +1,53 @@
 return {
-    ["basque"] = {name = "Euskara", code = "eu"},
-    ["catalan"] = {name = "Català", code = "ca", articles = {singular = {masc = "el", fem = "la"}, plural = {masc = "els", fem = "les"}}},
-    ["galician"] = {name = "Galego", code = "gl", articles = {singular = {masc = "o", fem = "a"}, plural = {masc = "os", fem = "as"}}},
-    ["italian"] = {name = "Italiano", code = "it"},
-    ["spanish"] = {name = "Castellano", code = "es", articles = {singular = {masc = "el", fem = "la"}, plural = {masc = "los", fem = "las"}}},
-    ["deutsch"] = {name = "Deutsche", code = "de"},
-    ["russian"] = {name = "Русский", code = "ru"},
-    ["french"] = {name = "Français", code = "fr", articles = {singular = {masc = "le", fem = "la"}, plural = {masc = "les", fem = "les"}}}
+    -- Langues avec articles
+    ["spanish"] = {name = "Español", code = "es", articles = {singular = {masc = "el", fem = "la"}, plural = {masc = "los", fem = "las"}}}, -- Plenement
+    ["french"] = {name = "Français", code = "fr", articles = {singular = {masc = "le", fem = "la"}, plural = {masc = "les", fem = "les"}}}, -- Plenement
+    ["portuguese_pt"] = {name = "Português (Portugal)", code = "pt-PT", articles = {singular = {masc = "o", fem = "a"}, plural = {masc = "os", fem = "as"}}}, -- Plenement
+    ["portuguese_br"] = {name = "Português (Brésil)", code = "pt-BR", articles = {singular = {masc = "o", fem = "a"}, plural = {masc = "os", fem = "as"}}}, -- Plenement
+    ["german"] = {name = "Deutsch", code = "de", articles = {singular = {masc = "der", fem = "die"}, plural = {masc = "die", fem = "die"}}}, -- Plenement
+    ["italian"] = {name = "Italiano", code = "it", articles = {singular = {masc = "il", fem = "la"}, plural = {masc = "i", fem = "le"}}}, -- Plenement
+    ["romanian"] = {name = "Română", code = "ro", articles = {singular = {masc = "un", fem = "o"}, plural = {masc = "niște", fem = "niște"}}}, -- Plenement
+    ["dutch"] = {name = "Nederlands", code = "nl", articles = {singular = {masc = "de", fem = "de"}, plural = {masc = "de", fem = "de"}}}, -- Plenement
+    ["greek"] = {name = "Ελληνικά", code = "el", articles = {singular = {masc = "ο", fem = "η"}, plural = {masc = "οι", fem = "οι"}}}, -- Plenement
+
+    -- Langues sans articles
+    ["mandarin"] = {name = "Mandarin", code = "zh"}, -- Plenement
+    ["english"] = {name = "English", code = "en"}, -- Plenement
+    ["hindi"] = {name = "Hindi", code = "hi"}, -- Plenement
+    ["arabic"] = {name = "العربية", code = "ar"}, -- Partiellement
+    ["bengali"] = {name = "বাংলা", code = "bn"}, -- Plenement
+    ["russian"] = {name = "Русский", code = "ru"}, -- Plenement
+    ["indonesian"] = {name = "Bahasa Indonesia", code = "id"}, -- Plenement
+    ["urdu"] = {name = "اردو", code = "ur"}, -- Plenement
+    ["japanese"] = {name = "日本語", code = "ja"}, -- Plenement
+    ["marathi"] = {name = "मराठी", code = "mr"}, -- Partiellement
+    ["telugu"] = {name = "తెలుగు", code = "te"}, -- Partiellement
+    ["turkish"] = {name = "Türkçe", code = "tr"}, -- Plenement
+    ["tamil"] = {name = "தமிழ்", code = "ta"}, -- Partiellement
+    ["vietnamese"] = {name = "Tiếng Việt", code = "vi"}, -- Plenement
+    ["korean"] = {name = "한국어", code = "ko"}, -- Plenement
+    ["persian"] = {name = "فارسی", code = "fa"}, -- Plenement
+    ["kannada"] = {name = "ಕನ್ನಡ", code = "kn"}, -- Partiellement
+    ["gujarati"] = {name = "ગુજરાતી", code = "gu"}, -- Partiellement
+    ["malayalam"] = {name = "മലയാളം", code = "ml"}, -- Partiellement
+    ["punjabi"] = {name = "ਪੰਜਾਬੀ", code = "pa"}, -- Partiellement
+    ["polish"] = {name = "Polski", code = "pl"}, -- Plenement
+    ["czech"] = {name = "Čeština", code = "cs"}, -- Plenement
+    ["hungarian"] = {name = "Magyar", code = "hu"}, -- Plenement
+    ["swedish"] = {name = "Svenska", code = "sv"}, -- Plenement
+    ["danish"] = {name = "Dansk", code = "da"}, -- Plenement
+    ["finnish"] = {name = "Suomi", code = "fi"}, -- Plenement
+    ["bulgarian"] = {name = "Български", code = "bg"}, -- Plenement
+    ["slovak"] = {name = "Slovenčina", code = "sk"}, -- Plenement
+    ["croatian"] = {name = "Hrvatski", code = "hr"}, -- Plenement
+    ["serbian"] = {name = "Српски", code = "sr"}, -- Plenement
+    ["norwegian"] = {name = "Norsk", code = "no"}, -- Plenement
+    ["lithuanian"] = {name = "Lietuvių", code = "lt"}, -- Plenement
+    ["latvian"] = {name = "Latviešu", code = "lv"}, -- Plenement
+    ["estonian"] = {name = "Eesti", code = "et"}, -- Plenement
+    ["icelandic"] = {name = "Íslenska", code = "is"}, -- Partiellement
+    ["maltese"] = {name = "Malti", code = "mt"}, -- Partiellement
+    ["kazakh"] = {name = "Қазақша", code = "kk"} -- Partiellement
 }
 
 --language codes can be found in https://cloud.google.com/translate/docs/languages
